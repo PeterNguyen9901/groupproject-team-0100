11048c2006ae56c00a0b9f334095a0a83fe4fef5
Sianna Mucino <s_mucino@u.pacific.edu>
2021-10-29 11:44:15 -0700

    weapons

---
+++ src/weapons.java
@@ -0,0 +1,50 @@
+
+public class weapons {
+	
+	double damage;
+	int level;
+	private weaponType weapon; 
+	
+	public enum weaponType {
+		Sword, Axe, Bow, Spear, Dagger
+	}
+	
+	
+	public double getDamage() {
+		return damage;
+		
+	}
+	
+	
+	public int getLevel() {
+		return level; 
+	}
+	
+	public int incrementLevel(int n) {
+		
+		return n += level;
+		
+	}
+	
+	public weaponType getType() {
+		
+		switch(weapon) {
+		case Sword:
+			return weaponType.Sword; 
+			
+		case Axe:
+			return weaponType.Axe;
+			
+		case Bow:
+			return weaponType.Bow;
+			
+		case Spear:
+			return weaponType.Spear;
+			
+		case Dagger:
+			return weaponType.Dagger;
+		}
+		return weapon; 
+	}
+	
+}
