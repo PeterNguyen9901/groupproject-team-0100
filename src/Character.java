1868fc140975f580a7afd80f1c812f9227b5f2d2
c6709 <c6709@10.15.26.238>
2021-10-30 09:46:21 -0700

    Character class being added

---
+++ src/Character.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+
+public class Character {
+	private int HP;
+	private int totalDamage;
+	private int gold;
+	private int level;
+	private int characterDeath;
+	private int monsterKilled;
+	ArrayList<weaponType> weaponList;
+	ArrayList<monsterType> monsterList;
+
+	public int getHp() {
+		return HP;
+	}
+
+
+	public void setHp(int HP) {
+		this.HP = HP;
+	}
+	public int gettotalDamage() {
+		return totalDamage;
+	}
+
+
+	public void setTotalDamage(int totalDamage) {
+		this.totalDamage = totalDamage;
+	}
+
+
+	public int getgold() {
+		return gold;
+	}
+
+
+	public void setgold(int gold) {
+		this.gold = gold;
+	}
+
+
+	public int getlevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public int getCharacterDeath() {
+		return characterDeath;
+	}
+
+
+	public void setCharacterDeath(int characterDeath) {
+		this.characterDeath = characterDeath;
+	}
+
+
+	public int getmonsterKilled() {
+		return monsterKilled;
+	}
+
+
+	public void setMonsterKilled(int monsterKilled) {
+		this.monsterKilled = monsterKilled;
+	}
+	
+	public void setWeaponsOwned() {
+		//Fill
+	}
+	public void setMonsterList() {
+		//Fill
+	}
+	public weaponType getWeaponList() {
+		
+		//Fill
+		return weaponType.SWORD;
+	}
+	public monsterType getMonsterList() {
+		
+		
+		//Fill
+		return monsterType.SLIME;
+		
+	}
+
+}
