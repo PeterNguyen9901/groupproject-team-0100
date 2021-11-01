0a9f2c2fbdc89317e3dc6e9afd9138db2acc6f2c
Marques <mparke23@STK-ECS-JR43343.stk.pacific.edu>
2021-11-01 11:54:28 -0700

    Marques - Cut up Character class into multiple classes

--- src/Character.java
+++ src/Character.java
@@ -1,63 +1,9 @@
 import java.util.ArrayList;
 //test
 public class Character {
-	private int HP;
-	private int totalDamage;
-	private int gold;
-	private int level;
-	private int characterDeath;
 	private int monsterKilled;
 	ArrayList<weaponType> weaponList;
 	ArrayList<monsterType> monsterList;
-
-	public int getHp() {
-		return HP;
-	}
-
-
-	public void setHp(int HP) {
-		this.HP = HP;
-	}
-	public int gettotalDamage() {
-		return totalDamage;
-	}
-
-
-	public void setTotalDamage(int totalDamage) {
-		this.totalDamage = totalDamage;
-	}
-
-
-	public int getgold() {
-		return gold;
-	}
-
-
-	public void setgold(int gold) {
-		this.gold = gold;
-	}
-
-
-	public int getlevel() {
-		return level;
-	}
-
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
-
-
-	public int getCharacterDeath() {
-		return characterDeath;
-	}
-
-
-	public void setCharacterDeath(int characterDeath) {
-		this.characterDeath = characterDeath;
-	}
-
-
 	public int getmonsterKilled() {
 		return monsterKilled;
 	}
@@ -68,7 +14,8 @@ public class Character {
 	}
 	
 	public void setWeaponsOwned() {
-		//Fill
+		//fill
+
 	}
 	public void setMonsterList() {
 		//Fill
