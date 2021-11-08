fe6a2d00c51a7f88f15fe29dde57460074fb88d0
Sianna Mucino <s_mucino@u.pacific.edu>
2021-11-08 11:23:04 -0800

    added the shop menu in MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -26,6 +26,14 @@ public class MainMenu implements ItemListener{
 		cards = new JPanel(new CardLayout(50,50));
 		cards.add(main);
 		pane.add(cards, BorderLayout.PAGE_END);
+		
+		
+		JFrame newFrame = new JFrame("Shop");  
+        MainMenu newGame = new MainMenu(); 
+        newGame.shop(newFrame.getContentPane());  
+        newFrame.pack(); 
+        newFrame.setVisible(true);  
+
 	}
 	private static void ShowGUI() {
 		JFrame frame = new JFrame("Click Click Go");
@@ -35,6 +43,28 @@ public class MainMenu implements ItemListener{
         frame.pack();
         frame.setVisible(true);
 	}
+	
+	
+	public void shop(Container pane) {
+		JPanel shop = new JPanel();
+		JLabel goldLabel, levelLabel;
+		cards = new JPanel(new CardLayout());
+		shop.add(new Button("Quit")); 
+		shop.add(new Button("Buy"));
+		
+		Border goldAndLevel = BorderFactory.createLineBorder(Color.black);
+		goldLabel = new JLabel(); 
+		goldLabel.setText("<html>Gold:   <br/>Level:   </html>"); 
+		goldLabel.setBorder(goldAndLevel);
+		shop.add(goldLabel);
+		 
+		cards = new JPanel(new CardLayout());
+		cards.add(shop);
+		
+		pane.add(cards, BorderLayout.CENTER); 
+	}
+
+	
 	public static void main(String[] args) {
 		//from example on documentation, but not sure if we need it
 		/*try {
