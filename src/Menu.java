8659629b8afe64f5d0ce72ea24a72e247e11695c
Thomas Lau <t_lau11@u.pacific.edu>
2021-11-12 12:09:33 -0800

    added gold to character and menu. anchored buttons to end of page in main menu

--- src/Menu.java
+++ src/Menu.java
@@ -12,7 +12,7 @@ public class Menu {
 	private JPanel shopPanel = new JPanel();
 	private JPanel startPanel = new JPanel();
 	CardLayout LAYOUT = new CardLayout();
-
+	Character player = new Character();
 	public Menu() {
 		deck.setLayout(LAYOUT);
 		
@@ -27,12 +27,23 @@ public class Menu {
         frame.setVisible(true);
 	}
 	public void MainMenu(Container pane) {
+		pane.setLayout(new BorderLayout()); //make pane border layout
+		int gold = player.getCurrency(); //will be used to create JLabel to show gold
+		JPanel BottomButtons = new JPanel(); //JPanel to hold buttons that will be on bottom of the screen
+		BottomButtons.setLayout(new GridLayout(1,3,1,1)); //makes JPanel a grid that is 1 by 3 and has a horizontal and vertical gap of 1
+		
+		//creates buttons
 		JButton MainShopButton = new JButton("Shop");
 		JButton MainFightButton = new JButton("Fight");
 		JButton MainStatsButton = new JButton("Stats");
-		pane.add(MainShopButton);
-		pane.add(MainFightButton);
-		pane.add(MainStatsButton);
+		
+		//add buttons to BottomButtons Panel
+		BottomButtons.add(MainShopButton);
+		BottomButtons.add(MainFightButton);
+		BottomButtons.add(MainStatsButton);
+		
+		//Add JPanel to the bottom of pane 
+		pane.add(BottomButtons, BorderLayout.PAGE_END);
 		
 		//action listener for shop button
 		MainShopButton.addActionListener(new ActionListener(){
